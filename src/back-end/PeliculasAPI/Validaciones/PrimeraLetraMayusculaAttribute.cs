7cc99a9f2293ae023c2a98df4b40c8e178e1ed92
Facundo <facundomarcos@live.com.ar>
2020-12-15 18:47:30 -0300

    Initial Commit

---
+++ src/back-end/PeliculasAPI/Validaciones/PrimeraLetraMayusculaAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeliculasAPI.Validaciones
+{
+    public class PrimeraLetraMayusculaAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            //el modelo ya esta validado desde el modelo con el [ApiController]
+           if (value == null || string.IsNullOrEmpty(value.ToString()))
+            {
+                return ValidationResult.Success;
+            }
+
+            var primeraletra = value.ToString()[0].ToString();
+
+            if (primeraletra != primeraletra.ToUpper())
+            {
+                return new ValidationResult("La primera letra debe ser mayúscula");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ src/front-end/src/app/actores/editar-actor/editar-actor.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-editar-actor',
+  templateUrl: './editar-actor.component.html',
+  styleUrls: ['./editar-actor.component.css']
+})
+export class EditarActorComponent implements OnInit {
+
+  //servicio para obtener el id
+  constructor(private activedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.activedRoute.params.subscribe(params => {
+      //alert(params.id)
+    })
+  }
+
+}
