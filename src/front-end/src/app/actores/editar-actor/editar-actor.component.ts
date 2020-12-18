fa0ab1261c261eda35d7cd3a145cacfed7a7a6d0
Facundo <facundomarcos@live.com.ar>
2020-12-18 02:20:51 -0300

    55 - componente de selector de imagenes

--- src/front-end/src/app/actores/editar-actor/editar-actor.component.ts
+++ src/front-end/src/app/actores/editar-actor/editar-actor.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { actorCreacionDTO } from '../actor';
+import { actorDTO } from '../actor';
 
 @Component({
   selector: 'app-editar-actor',
@@ -12,7 +13,7 @@ export class EditarActorComponent implements OnInit {
   //servicio para obtener el id
   constructor(private activedRoute: ActivatedRoute) { }
 
-  modelo: actorCreacionDTO ={nombre: 'Facundo', fechaNacimiento: new Date()}
+  modelo: actorDTO = {nombre: 'Bruce Willis', fechaNacimiento: new Date(), foto: 'https://m.media-amazon.com/images/M/MV5BMjA0MjMzMTE5OF5BMl5BanBnXkFtZTcwMzQ2ODE3Mw@@._V1_UY317_CR27,0,214,317_AL_.jpg'}
 
   ngOnInit(): void {
     this.activedRoute.params.subscribe(params => {
