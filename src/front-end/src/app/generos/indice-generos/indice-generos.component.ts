f61e8356b3caadb967e0729967462b583bf83aa3
Facundo <facundomarcos@live.com.ar>
2020-12-22 02:30:12 -0300

    95 - Paginacion Angular

--- src/front-end/src/app/generos/indice-generos/indice-generos.component.ts
+++ src/front-end/src/app/generos/indice-generos/indice-generos.component.ts
@@ -1,4 +1,6 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { generoDTO } from '../genero';
 import { GenerosService } from '../generos.service';
 
@@ -13,14 +15,28 @@ export class IndiceGenerosComponent implements OnInit {
 
   generos: generoDTO[];
   columnasAMostrar = ['id', 'nombre', 'acciones'];
+  cantidadTotalRegistros;
+  paginaActual = 1;
+  cantidadRegistrosAMostrar = 10;
 
   ngOnInit(): void {
-    this.generosService.obtenerTodos()
-    .subscribe(generos => {
-      this.generos = generos;
-    },
-    error => console.error(error));
+    this.cargarRegistros(this.paginaActual, this.cantidadRegistrosAMostrar);
     
   }
 
+  cargarRegistros(pagina: number, cantidadElementosAMostrar){
+    this.generosService.obtenerTodos(pagina, cantidadElementosAMostrar)
+    .subscribe((respuesta: HttpResponse<generoDTO[]>) => {
+      this.generos = respuesta.body;
+      //console.log(respuesta.headers.get("cantidadTotalRegistros"));
+      this.cantidadTotalRegistros = respuesta.headers.get("cantidadTotalRegistros");
+    },
+    error => console.error(error));
+  }
+
+  actualizarPaginacion(datos: PageEvent){
+    this.paginaActual = datos.pageIndex + 1;
+    this.cantidadRegistrosAMostrar = datos.pageSize;
+    this.cargarRegistros(this.paginaActual, this.cantidadRegistrosAMostrar);
+  }
 }
