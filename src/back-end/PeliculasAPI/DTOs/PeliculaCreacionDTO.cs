277a80545741882e2d73696253d6c4775bf4d02b
Facundo <facundomarcos@live.com.ar>
2020-12-27 22:09:26 -0300

    109 - crear pliculas web API

---
+++ src/back-end/PeliculasAPI/DTOs/PeliculaCreacionDTO.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PeliculasAPI.Utilidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeliculasAPI.DTOs
+{
+    public class PeliculaCreacionDTO
+    {
+        [Required]
+        [StringLength(maximumLength: 300)]
+        public string Titulo { get; set; }
+        public string Resumen { get; set; }
+        public string Trailer { get; set; }
+        public bool EnCines { get; set; }
+        public DateTime FechaLanzamiento { get; set; }
+        public IFormFile Poster { get; set; }
+
+        //para recibir el listado de generos
+        [ModelBinder(BinderType = typeof(TypeBinder<List<int>>))]
+        public List<int> GenerosIds { get; set; }
+
+        //para recibir el listado de cines
+        [ModelBinder(BinderType = typeof(TypeBinder<List<int>>))]
+        public List<int> CinesIds { get; set; }
+
+        //el tipo actor es mas complejo xq 
+        //tambien incluye al personaje y el orden de importancia en la pelicula
+        [ModelBinder(BinderType = typeof(TypeBinder<List<ActorPeliculaCreacionDTO>>))]
+        public List<ActorPeliculaCreacionDTO> Actores { get; set; }
+}
+}
